e654cb7064b7e071fbeda87538a7e648ccf0273c
fluidbyte <kent@fluidbyte.net>
2017-05-09 16:57:36 -0500

    feat(plugins): adds handler to load plugin

--- README.md
+++ README.md
@@ -21,3 +21,18 @@ fixd.addObject('foo', { foo: 'bar' })
 // Create a new instance of the fixture
 const fooFixture = fixd.create('foo')
 ```
+
+## Plugins
+
+Fixd uses plugins to allow the developer to specify which type of fixtures to 
+use. At initialization you must specify which plugins you wish to utilize. From 
+the example above:
+
+```javascript
+fixd.use('object')
+```
+
+The `use` method will use a built-in (single string), a path to a custom plugin, 
+or an already `require`'d plugin object.
+
+Custom plugins must be an object with two methods: `add` and `create`
--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-const fs = require('fs')
 const path = require('path')
 
 const fixd = {
@@ -8,27 +7,42 @@ const fixd = {
   fixtures: {},
   
   /**
+   * Attempts to load (or returns) plugin
+   * @param {Object|String} plugin The plugin object, name, or path
+   * @returns {Object}
+   */
+  getPlugin: (plugin) => {
+    if (typeof plugin === 'string') {
+      try {
+        const pluginPath = path.indexOf('/') >= 0 
+          ? plugin // Try user-supplied path
+          : path.resolve(__dirname, `./plugins/${plugin}.js`) // Use built-in
+        return require(pluginPath)
+      } catch (e) {
+        throw new Error(`Could not load plugin ${plugin}`)
+      }
+    } else if (plugin.add && plugin.create) {
+      return plugin
+    } else {
+      throw new Error('Invalid plugin supplied, please see documentation')
+    }
+  }
+  
+  /**
    * Instructs fixd to use plugin, creates addPLUGIN method
-   * @param {Object} plugin The plugin object to apply
+   * @param {Object|String} plugin The plugin name or object to apply
    */
   use: function (plugin) {
-    const pluginPath = path.resolve(__dirname, `./plugins/${plugin}.js`)
-    try {
-      fs.statSync(pluginPath)
-      fixd[`add${plugin.charAt(0).toUpperCase() + plugin.slice(1)}`] = (name, ...args) => {
-        // Validations
-        if (!name || typeof name !== 'string') throw new Error('Must supply a (string) name value for the fixture')
-        if (fixd.fixtures[name]) throw new Error(`Fixture ${name} already exists, must be unique`)
-
-        // Setup fixture
-        const plug = require(pluginPath)
-        fixd.fixtures[name] = {
-          data: plug.add(...args),
-          create: plug.create
-        }
+    const plug = fixd.getPlugin(plugin)
+    fixd[`add${plugin.charAt(0).toUpperCase() + plugin.slice(1)}`] = (name, ...args) => {
+      // Validations
+      if (!name || typeof name !== 'string') throw new Error('Must supply a (string) name value for the fixture')
+      if (fixd.fixtures[name]) throw new Error(`Fixture ${name} already exists, must be unique`)
+      // Setup fixture
+      fixd.fixtures[name] = {
+        data: plug.add(...args),
+        create: plug.create
       }
-    } catch (e) {
-      throw new Error(`Could not load plugin ${plugin}`)
     }
   },
   
