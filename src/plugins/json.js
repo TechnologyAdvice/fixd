241d376cffdaae78a46507622b5c8ad157f85648
fluidbyte <kent@fluidbyte.net>
2017-05-09 16:32:54 -0500

    feat(json): adds JSON plugin

---
+++ src/plugins/json.js
@@ -0,0 +1,25 @@
+module.exports = {
+  /**
+   * Adds a JSON object as a fixture
+   * @param {String} json The JSON object to set as default fixture
+   * @returns {Object}
+   */
+  add: (json) => {
+    try {
+      return JSON.parse(json)
+    } catch (e) {
+      throw new Error(`Must supply a valid JSON object`)
+    }
+  },
+  
+  /**
+   * Creates a new instance of the fixture with any required modifications
+   * @param {Object} data The original fixture data
+   * @param {Object} mods Any modification for this specific instance
+   * @returns {String}
+   */
+  create: (data, mods = {}) => {
+    if (typeof mods !== 'object') throw new Error(`Must supply a valid object`)
+    return JSON.stringify(Object.assign({}, data, mods))
+  }
+}
