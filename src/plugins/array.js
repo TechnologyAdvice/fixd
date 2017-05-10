8fde2195dfa62232d9f314fa790b80da589a1e21
fluidbyte <kent@fluidbyte.net>
2017-05-10 08:08:51 -0500

    feat(plugins): adds array plugin

---
+++ src/plugins/array.js
@@ -0,0 +1,28 @@
+module.exports = {
+  /**
+   * @property {String} plugin name
+   */
+  name: 'array',
+  /**
+   * Adds an array as a fixture
+   * @param {Array} array The array to set as default fixture
+   * @returns {Array}
+   */
+  add: (array) => {
+    if (!Array.isArray(array)) throw new Error(`Must supply a valid array`)
+    return array
+  },
+
+  /**
+   * Creates a new instance of the fixture with any required modifications
+   * @param {Array} data The original fixture data
+   * @param {Array} mods Any modification for this specific instance
+   * @returns {Array}
+   */
+  create: (data, mods = []) => {
+    if (!Array.isArray(mods)) throw new Error(`Must supply a valid array`)
+    const arr = [ ...data ]
+    if (mods.length) return arr.concat(mods)
+    return arr
+  }
+}
