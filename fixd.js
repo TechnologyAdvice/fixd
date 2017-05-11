5790d10bb3a77d4b22a6109695665141b1a715d7
fluidbyte <kent@fluidbyte.net>
2017-05-11 11:49:01 -0500

    feat(plugins): auto-load built-in plugins

--- fixd.js
+++ fixd.js
@@ -1,1 +1,14 @@
-module.exports = require('./src/index')
+const fs = require('fs')
+const path = require('path')
+const fixd = require('./src/index')
+
+// Auto-load built-in plugins
+try {
+  fs.readdirSync(path.resolve(__dirname, './src/plugins')).forEach((f) => {
+    fixd.use(f.replace('.js', ''))
+  })
+} catch (e) {
+  throw new Error('Error loading built-in plugins', e.message)
+}
+
+module.exports = fixd
