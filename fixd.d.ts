bd49dcd197f181651083b10084faf82ac00c4c9a
Elliott Campbell <elliott.campbell@banno.com>
2019-08-30 14:33:25 -0500

    Added final type declarations

--- fixd.d.ts
+++ fixd.d.ts
@@ -1,16 +1,34 @@
-// Type definitions for fixd 1.0
-// Project: https://github.com/TechnologyAdvice/fixd
-// Definitions by: Elliott Campbell <https://github.com/ElliottCampbellJHA?
-//                 Eric Heikes <https://github.com/eheikes>
+declare const fixd: {
+  /**
+   * Deeply freezes an object
+   * @param {*} obj The object to freeze
+   * @returns {*}
+   */
+  readonly freeze: (obj: any) => any;
+  /**
+   * Retrieves a value from the fixd library
+   * @param {Object} target Target object to retrieve from
+   * @param {String} name Property name to retrieve
+   * @returns {*} The value of the property
+   */
+  readonly get: (target: any, name: string | symbol) => any;
+  /**
+   * Add a property to the fixd library and freezes if not in namespace
+   * @param {Object} obj The object to set
+   * @param {String} prop A string name to represent the fixture
+   * @param {*} value The value to set
+   * @returns {Boolean}
+   */
+  readonly set: (obj: any, prop: string | symbol, value: any) => boolean;
+  /**
+   * Returns a fixture and allows for (option) modification
+   * @param {String} name The namespace of the fixture to mutate
+   * @param {Function} modifier A function to modify the fixture
+   * @returns {*} Unreferenced, immutable copy of object
+   */
+  readonly create: (name: string | symbol, modifier: (obj: any) => any) => any;
 
-declare module 'fixd' {
-  const fixd: { [key: string]: any }
-  export = fixd
+  [key: string]: any;
+};
 
-  // namespace fixd {
-  //   function freeze(obj: Object): Object
-  //   function get(target: Object, name: String): Object
-  //   function set(obj: Object, prop: String, value: Object): Boolean
-  //   function create(name: String, modifier: Function): Object
-  // }
-}
+export = fixd
