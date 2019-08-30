6bd2efa3b846f558b8f3fbb934de864cca304680
Elliott Campbell <elliott.campbell@banno.com>
2019-08-30 12:54:23 -0500

    Initial type declarations added

---
+++ fixd.d.ts
@@ -0,0 +1,16 @@
+// Type definitions for fixd 1.0
+// Project: https://github.com/TechnologyAdvice/fixd
+// Definitions by: Elliott Campbell <https://github.com/ElliottCampbellJHA?
+//                 Eric Heikes <https://github.com/eheikes>
+
+declare module 'fixd' {
+  const fixd: { [key: string]: any }
+  export = fixd
+
+  // namespace fixd {
+  //   function freeze(obj: Object): Object
+  //   function get(target: Object, name: String): Object
+  //   function set(obj: Object, prop: String, value: Object): Boolean
+  //   function create(name: String, modifier: Function): Object
+  // }
+}
